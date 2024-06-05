4c17731fc4cffd3e25331460dca1d73816c4befc
spiroskonstantinidis <60156291+spiros-k@users.noreply.github.com>
2024-06-05 20:34:51 +0300

    final stage

--- index.js
+++ index.js
@@ -1,50 +1,170 @@
 
 const arrayChoises = ["rock", "paper", "scissors"];
-let randomChoise = arrayChoises[Math.floor(Math.random() * arrayChoises.length)];
 let userChoise = "";
-const roundFinishMessage = document.createElement("h3")
+let botChoise;
+let playerOnePoints = 0;
+let playerTwoPoints = 0;
 
-console.log(randomChoise)
-
-function controlOfGame() {
-    if(randomChoise === "rock"){
+// bots random button choise
+function botsSelectedBtn() {
+    if(botChoise === "rock"){
         document.getElementById("rock").classList.add("selected")
-        if(userChoise === "rock"){
-            let roundMessage = document.createTextNode("It's a draw")
-            roundFinishMessage.appendChild(roundMessage)
-            let messageDiv = document.getElementById("message-div")
-            document.body.insertBefore(roundFinishMessage, messageDiv)
-            setTimeout(() => {
-                messageDiv.remove()
-            }, 3000)
-        } else if(userChoise === "paper"){
-            let roundMessage = document.createTextNode("Paper wins Rock")
-            roundFinishMessage.appendChild(roundMessage)
-            let messageDiv = document.getElementById("message-div")
-            document.body.insertBefore(roundFinishMessage, messageDiv)
-            setTimeout(() => {
-                messageDiv.remove()
-            }, 3000)
-        } else if(userChoise === "scissors"){
-            let roundMessage = document.createTextNode("Rock wins Scissors")
-            roundFinishMessage.appendChild(roundMessage)
-            let messageDiv = document.getElementById("message-div")
-            document.body.insertBefore(roundFinishMessage, messageDiv)
-            setTimeout(() => {
-                messageDiv.remove()
-            }, 3000)
-        }
+        setTimeout(() => {
+            document.getElementById("rock").classList.remove("selected")
+        }, 2500)
+    } else if(botChoise === "paper"){
+        document.getElementById("paper").classList.add("selected")
+        setTimeout(() => {
+            document.getElementById("paper").classList.remove("selected")
+        }, 2500)
+    } else if(botChoise === "scissors"){
+        document.getElementById("scissors").classList.add("selected")
+        setTimeout(() => {
+            document.getElementById("scissors").classList.remove("selected")
+        }, 2500)
+    } else{
+        console.log("Something went wrong.")
+    }
+}
+
+// users button choise
+function usersSelectedBtn() {
+    if(userChoise === "rock"){
+        document.getElementById("rock").classList.add("selected")
+        setTimeout(() => {
+            document.getElementById("rock").classList.remove("selected")
+        }, 2500)
+    } else if(userChoise === "paper"){
+        document.getElementById("paper").classList.add("selected")
+        setTimeout(() => {
+            document.getElementById("paper").classList.remove("selected")
+        }, 2500)
+    } else if(userChoise === "scissors"){
+        document.getElementById("scissors").classList.add("selected")
+        setTimeout(() => {
+            document.getElementById("scissors").classList.remove("selected")
+        }, 2500)
+    } else{
+        console.log("Something went wrong.")
     }
 }
 
 
+const roundFinishMessage = document.createElement("h2")
+// message if draw
+function drawMessage() {
+    roundFinishMessage.innerHTML = "Its a draw!";
+    roundFinishMessage.style.color = "#fafafa";
+    document.getElementById("message-div").appendChild(roundFinishMessage)
+}
+
+// message if p1 wins
+function winMessage() {
+    roundFinishMessage.innerHTML = "Player 1 wins!";
+    roundFinishMessage.style.color = "#fafafa";
+    document.getElementById("message-div").appendChild(roundFinishMessage)
+}
+
+
+// message if p2 wins
+function loseMessage() {
+    roundFinishMessage.innerHTML = "Player 2 wins!";
+    roundFinishMessage.style.color = "#fafafa";
+    document.getElementById("message-div").appendChild(roundFinishMessage)
+}
+
+
+// rock button clicked
 document.getElementById("rock").addEventListener("click", () => {
-    document.getElementById("rock").classList.add("selected")
-    document.getElementById("paper").classList.add("not-selected")
-    document.getElementById("scissors").classList.add("not-selected")
+    botChoise = arrayChoises[Math.floor(Math.random() * arrayChoises.length)];
+    console.log(botChoise)
     userChoise = "rock";
+
+    usersSelectedBtn()
+    botsSelectedBtn()
     
-controlOfGame()
+    if(userChoise === botChoise){
+        drawMessage()
+        setTimeout(() => {
+            roundFinishMessage.remove()
+        }, 1750)
+    } else if(botChoise === "paper"){
+        loseMessage()
+        playerTwoPoints++
+        setTimeout(() => {
+            roundFinishMessage.remove()
+        }, 1750)
+    } else if(botChoise === "scissors"){
+        winMessage()
+        playerOnePoints++
+        setTimeout(() => {
+            roundFinishMessage.remove()
+        }, 1750)
+    }
+    document.getElementById("player-one-points").innerHTML = playerOnePoints
+    document.getElementById("player-two-points").innerHTML = playerTwoPoints
 })
 
 
+// paper button clicked
+document.getElementById("paper").addEventListener("click", () => {
+    botChoise = arrayChoises[Math.floor(Math.random() * arrayChoises.length)];
+    console.log(botChoise)
+    userChoise = "paper";
+
+    usersSelectedBtn()
+    botsSelectedBtn()
+    
+    if(userChoise === botChoise){
+        drawMessage()
+        setTimeout(() => {
+            roundFinishMessage.remove()
+        }, 1750)
+    } else if(botChoise === "rock"){
+        winMessage()
+        playerOnePoints++
+        setTimeout(() => {
+            roundFinishMessage.remove()
+        }, 1750)
+    } else if(botChoise === "scissors"){
+        loseMessage()
+        playerTwoPoints++
+        setTimeout(() => {
+            roundFinishMessage.remove()
+        }, 1750)
+    }
+    document.getElementById("player-one-points").innerHTML = playerOnePoints
+    document.getElementById("player-two-points").innerHTML = playerTwoPoints
+})
+
+
+// scissors button clicked
+document.getElementById("scissors").addEventListener("click", () => {
+    botChoise = arrayChoises[Math.floor(Math.random() * arrayChoises.length)];
+    console.log(botChoise)
+    userChoise = "scissors";
+
+    usersSelectedBtn()
+    botsSelectedBtn()
+
+    if(userChoise === botChoise){
+        drawMessage()
+        setTimeout(() => {
+            roundFinishMessage.remove()
+        }, 1750)
+    } else if(botChoise === "rock"){
+        loseMessage()
+        playerTwoPoints++
+        setTimeout(() => {
+            roundFinishMessage.remove()
+        }, 1750)
+    } else if(botChoise === "paper"){
+        winMessage()
+        playerOnePoints++   
+        setTimeout(() => {
+            roundFinishMessage.remove()
+        }, 1750) 
+    }
+    document.getElementById("player-one-points").innerHTML = playerOnePoints
+    document.getElementById("player-two-points").innerHTML = playerTwoPoints
+})
